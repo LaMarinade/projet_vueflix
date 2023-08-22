44a32fa333c182b8bf39fd75c3e430c1d1bd0a5f
Marine Valentin <Lamarine@MacBook-Air.local>
2023-08-22 14:24:33 +0200

    cards

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,14 +1,12 @@
 <template>
- <div class="w-16 h-16 mr-4 shadow-md bg-white flex">
-  <img class= "rounded-full" :src="profileImage" alt="Photo de profil" />
+    <div class="white-test">
+      <img class= "w-16 h-16 rounded-full" :src="profileImage" alt="Photo de profil" />
+        <h2 class="text-lg font-semibold">{{ name }}</h2>
+          <p class="text-gray-600">{{ role }}</p>
+          <button @click="showBio" class="mt-2 text-blue-600 hover:underline">Voir la biographie</button>
+          <p v-if="showingBio" class="mt-2">{{ bio }}</p>
+          </div>
 
-    <!-- <div class="flex flex-col"> -->
-      <h2 class="text-lg font-semibold">{{ name }}</h2>
-      <p class="text-gray-600">{{ role }}</p>
-      <button @click="showBio" class="mt-2 text-blue-600 hover:underline">Voir la biographie</button>
-      <p v-if="showingBio" class="mt-2">{{ bio }}</p>
-    </div>
-<!-- </div> -->
 </template>
 
 <script setup>
@@ -27,3 +25,13 @@ const showBio = () => {
   showingBio.value = !showingBio.value;
 };
 </script>
+
+<style scoped>
+
+.white-test{
+  background-color: white;
+  width: 400px;
+  height: 200px;
+  margin: 10px;
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,23 +1,37 @@
 <template>
-    <div class="container mx-auto p-8 flex flex-col items-center">
-    <h1 class="text-2xl font-semibold mb-4">Notre Équipe</h1>
-    <!-- <div class="grid grid-cols-1 md:grid-cols-3 gap-4"> -->
-        <Card
-          v-for="(member, index) in teamMembers"
-          :key="index"
-          :profileImage="member.profileImage"
-          :name="member.name"
-          :role="member.role"
-          :bio="member.bio"
-        />
-      </div>
-    <!-- </div> -->
+        <h1 class="text-2xl font-semibold mx-auto mt-4 text-center">Notre Équipe</h1>
+        <!-- <div class="grid grid-cols-1 md:grid-cols-3 gap-4"> -->
+            <div class=" container mx-auto p-8 flex items-center">
+
+                <Card
+              v-for="(member, index) in teamMembers"
+              :key="index"
+              :profileImage="member.profileImage"
+              :name="member.name"
+              :role="member.role"
+              :bio="member.bio"
+            />
+
+            </div>
+        
   </template>
   
   <script setup>
   import Card from '../components/Card.vue';
   
   const teamMembers = [
+    {
+      profileImage: '../img/avatar-vueflix.png',
+      name: 'Marinade',
+      role: 'Manager',
+      bio: 'Biographie du manager...'
+    },
+    {
+      profileImage: '../img/avatar-vueflix.png',
+      name: 'Marinade',
+      role: 'Manager',
+      bio: 'Biographie du manager...'
+    },
     {
       profileImage: '../img/avatar-vueflix.png',
       name: 'Marinade',
